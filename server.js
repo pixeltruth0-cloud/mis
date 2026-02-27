c3a34b249141a2ba45511767ac956621c0ea28fa
pixeltruth0-cloud <pixeltruth0@gmail.com>
2026-02-27 16:18:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const upload = multer();
 
 /* ======================
    Middleware
-====================== */
+====================== *//
 app.use(cors({
   origin: "https://pixeltruth.com",
   credentials: true
